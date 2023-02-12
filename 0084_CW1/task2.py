3d7764f07cdde6e66725e9816819a90ec3f2cfb6
Chongfeng Ling <ling.chongfeng@outlook.com>
2023-02-12 10:47:04 +0000

    t2: added simple Inverted index

--- 0084_CW1/task2.py
+++ 0084_CW1/task2.py
@@ -13,3 +13,19 @@ def load_terms(file_path='0084_CW1/terms_kept.txt'):
         lines = f.readlines()
     terms = [line.strip() for line in lines]
     return terms
+
+def II_simple(terms, document):
+    II_simple_dict = {key: [] for key in terms}
+    for (qid, pid, passage) in tqdm(zip(document['qid'], document['pid'], document['passage'])):
+        passage_list = set(passage.split())
+        for word in passage_list:
+            if word in II_simple_dict.keys():
+                II_simple_dict[word].append((qid, pid))
+    return II_simple_dict
+
+if __name__=='__main__':
+    document = load_document()
+    terms = load_terms()
+    res = II_simple(terms=terms, document=document)
+    print(res)
+    pass
