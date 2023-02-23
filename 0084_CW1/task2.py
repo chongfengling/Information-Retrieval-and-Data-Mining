11a8bab3665faa8bc81b4902965bcd1f6338ff2f
Chongfeng Ling <ling.chongfeng@outlook.com>
2023-02-23 21:16:34 +0000

    followed the tokenisation method from task1

--- 0084_CW1/task2.py
+++ 0084_CW1/task2.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from collections import Counter
 from tqdm import tqdm
+from task1 import tokenisation
 
 def load_document(file_path='0084_CW1/candidate-passages-top1000.tsv'):
     df = pd.read_csv(file_path, sep='\t', names=['qid', 'pid', 'query', 'passage'])
@@ -43,7 +44,7 @@ def II_simple(terms, document):
     """
     II_simple_dict = {key: [] for key in terms}
     for (qid, pid, passage) in tqdm(zip(document['qid'], document['pid'], document['passage']), desc='II_simple'):
-        passage_set = set(passage.split())
+        passage_set = set(tokenisation(passage))
         for word in passage_set:
             if word in II_simple_dict.keys():
                 II_simple_dict[word].append((qid, pid))
@@ -66,7 +67,7 @@ def II_counts(terms, document):
     """
     II_counts_dict = {key: [] for key in terms}
     for (qid, pid, passage) in tqdm(zip(document['qid'], document['pid'], document['passage']), desc='II_counts'):
-        passage_list = passage.split()
+        passage_list = tokenisation(passage)
         passage_set = set(passage.split())
         for word in passage_set:
             if word in II_counts_dict.keys():
@@ -91,7 +92,7 @@ def II_positions(terms, document):
     """
     II_positions_dict = {key: [] for key in terms}
     for (qid, pid, passage) in tqdm(zip(document['qid'], document['pid'], document['passage']), desc='II_positions'):
-        passage_list = passage.split()
+        passage_list = tokenisation(passage)
         passage_set = set(passage.split())
         for word in passage_set:
             if word in II_positions_dict.keys():
@@ -100,12 +101,12 @@ def II_positions(terms, document):
     return II_positions_dict
 
 if __name__=='__main__':
-    document = load_document()
-    terms = load_terms()
+    document = load_document(file_path='0084_CW1/candidate-passages-top1000.tsv')
+    terms = load_terms(file_path='0084_CW1/terms_kept.txt')
     # res = II_simple(terms=terms, document=document)
     # print(res)
-    # res = II_counts(terms=terms, document=document)
+    res = II_counts(terms=terms, document=document)
 
-    res = II_positions(terms=terms, document=document)
+    # res = II_positions(terms=terms, document=document)
     
     pass
