daeffd02ae9861c329a87cc96806c85fda1a9f88
Chongfeng Ling <ling.chongfeng@outlook.com>
2023-02-24 13:46:08 +0000

    calculated TFIDF for given documents and queries

--- 0084_CW1/task3.py
+++ 0084_CW1/task3.py
@@ -33,6 +33,43 @@ def IDF(document: pd.DataFrame, terms: list):
     IDF_ts = np.log10(N / n_terms)
     return IDF_ts
 
+def TF_IDF(document: pd.DataFrame, query: pd.DataFrame, terms: list, IDF_ts: np.array, save_raw:bool=True, save_top:bool=True):
+    """TF_t,d = Term frequency of term t in document d
+    TF_t,d = 
+
+    Parameters
+    ----------
+    document : pd.DataFrame
+        _description_
+    query : pd.DataFrame
+        _description_
+    terms : list
+        _description_
+    IDF_ts : np.array
+        Inverse Document Frequency of terms in a document collection
+    save_raw : bool
+        save the raw TFIDF data or not
+    save_top : bool
+        save the sorted top100 TFIDF data or not
+    """
+    
+    # II_counts_dict = II_counts(terms=terms, document=document)
+    tmp = []
+    # df_score = pd.DataFrame(names=['qid', 'pid', 'score'])
+    for (qid, query) in tqdm(zip(query['qid'], query['query']), desc="Get TF-IDF for query"):
+        qid_terms = set(tokenisation(query))
+        passages = document.loc[document['qid'] == qid]
+        common_term = [term for term in qid_terms if term in terms]
+        for (pid, passage) in zip(passages['pid'], document['passage']):
+            score = 0.0
+            for term in common_term:
+                IDF_t = IDF_ts[terms.index(term)]
+                tf_t = passage.count(term)
+                score += IDF_t * tf_t
+            tmp.append([qid, pid, score])
+    df_score = pd.DataFrame(tmp, columns =['qid', 'pid', 'score'], dtype = float).astype(dtype = {'qid': int, 'pid': int, 'score': float})
+    select_top_passages(df_score, save_raw=save_raw, save_top=save_top)
+    
 def select_top_passages(df_raw:pd.DataFrame, save_raw:bool=True, save_top:bool=True):
     # name the output files by current time
     import datetime
