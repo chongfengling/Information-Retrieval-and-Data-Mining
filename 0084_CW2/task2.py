1515c0085645b36517ebf016c67f673a5323ae4a
Chongfeng Ling <ling.chongfeng@outlook.com>
2023-03-15 14:58:54 +0000

    ✨ feat(t2): add basic class layout for LR

--- 0084_CW2/task2.py
+++ 0084_CW2/task2.py
@@ -1,1 +1,18 @@
-# implementation of logistic regression model
+from gensim.models import word2vec
+
+
+class LogisticRegression:
+    def __init__(self) -> None:
+        pass
+
+    def fit():
+        pass
+
+    def predict():
+        pass
+
+
+
+    def _negative_sampling():
+        """negative sampling"""
+        pass
