6b38e5884356121cf29e2aed89607ceb7ce00fad
Chongfeng Ling <ling.chongfeng@outlook.com>
2023-03-18 20:05:24 +0000

    ✨ feat(t3): add model config

--- 0084_CW2/task3.py
+++ 0084_CW2/task3.py
@@ -6,6 +6,12 @@ from BM25 import tokenisation
 from task1 import mAP, mNDCG
 
 if __name__=="__main__":
+    # config the parameters
+    etas = [0.1, 0.2, 0.3, 0.4, 0.5]
+    max_depths = [3, 4, 5, 6, 7]
+    num_boost_rounds = [10, 20, 30, 40, 50]
+    objective = 'rank:pairwise'
+
     # construct the training, validation and testing datasets
     Xy_train_arr = np.load('input_df_train_sub.npy')
     Xy_train = xgb.DMatrix(Xy_train_arr[:, 2:-1], label=Xy_train_arr[:, -1])
@@ -19,3 +25,13 @@ if __name__=="__main__":
     Xy_test = xgb.DMatrix(Xy_test_arr[:, 2:-1], label=Xy_test_arr[:, -1])
     Xy_test_qid, Xy_test_pid = Xy_test_arr[:, 0], Xy_test_arr[:, 1]
 
+    for eta in etas:
+        for max_depth in max_depths:
+            for num_boost_round in num_boost_rounds:
+                params = {
+                    'objective': objective,
+                    'eta': eta,
+                    'max_depth': max_depth,
+                    'num_boost_round': num_boost_round
+                }
+                pass
